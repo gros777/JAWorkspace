68ce583dbbbcf50824bbaef8e3d637cc932ba3ca
Victor A. Hernández <gros777@gmail.com>
2013-04-01 19:12:00 -0600

    Using model driven approach on AddNewProductAction

--- producto-register-app/src/main/java/me/victorhernandez/struts2/pra/actions/AddProductAction.java
+++ producto-register-app/src/main/java/me/victorhernandez/struts2/pra/actions/AddProductAction.java
@@ -1,6 +1,5 @@
 package me.victorhernandez.struts2.pra.actions;
 
-import java.util.Date;
 
 import me.victorhernandez.struts2.pra.domanin.Product;
 import me.victorhernandez.struts2.pra.service.ProductService;
@@ -9,69 +8,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
 
-public class AddProductAction extends ActionSupport {
+public class AddProductAction extends ActionSupport implements ModelDriven<Product>{
 
 	
-	private String productName;
-	private Date goodThough;
-	private int availables;
-	private String mark;
-	private String shelve;
-	private int version;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6727629495086452702L;
 	
+	Product newProduct = new Product();
 	@Autowired
 	@Qualifier(value="productService")
 	private ProductService productService;
 	
-	public String getProductName() {
-		return productName;
-	}
-
-	public void setProductName(String productName) {
-		this.productName = productName;
-	}
-
-	public Date getGoodThough() {
-		return goodThough;
-	}
-
-	public void setGoodThough(Date goodThough) {
-		this.goodThough = goodThough;
-	}
-
-	public int getAvailables() {
-		return availables;
-	}
-
-	public void setAvailables(int availables) {
-		this.availables = availables;
-	}
-
-	public String getMark() {
-		return mark;
-	}
-
-	public void setMark(String mark) {
-		this.mark = mark;
-	}
-
-	public String getShelve() {
-		return shelve;
-	}
-
-	public void setShelve(String shelve) {
-		this.shelve = shelve;
-	}
-
-	public int getVersion() {
-		return version;
-	}
-
-	public void setVersion(int version) {
-		this.version = version;
-	}
-
 	public ProductService getProductService() {
 		return productService;
 	}
@@ -79,17 +30,16 @@ public class AddProductAction extends ActionSupport {
 	public void setProductService(ProductService productService) {
 		this.productService = productService;
 	}
+	
+	public Product getModel() {
+		return newProduct;
+	}
 
 	public String execute() throws Exception{
-		Product newProduct = new Product();
-		newProduct.setVersion(1);
-		newProduct.setAvailables(availables);
-		newProduct.setGoodThough(goodThough);
-		newProduct.setMark(mark);
-		newProduct.setProductName(productName);
-		newProduct.setShelve(shelve);
 		productService.persistProduct(newProduct);
 		
 		return SUCCESS;
 	}
+
+	
 }
